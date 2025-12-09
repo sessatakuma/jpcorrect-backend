8c348915465055423e0078cff89624f833ab3f98
Wei De Chien <wade00754@gmail.com>
2025-12-09 23:43:06 +0800

    refactor(api.go): improve server initialization and shutdown handling

--- internal/cmd/api.go
+++ internal/cmd/api.go
@@ -29,12 +29,14 @@ func Execute() {
 	api.Register(r, a)
 
 	srv := &http.Server{
-		Addr:    ":8080",
-		Handler: r.Handler(),
+		Addr:              ":8080",
+		Handler:           r,
+		ReadHeaderTimeout: 5 * time.Second,
 	}
 
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
 	go func() {
-		// service connections
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("listen: %s\n", err)
 		}
@@ -50,10 +52,13 @@ func Execute() {
 	<-quit
 	log.Println("Shutting down server...")
 
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
-		log.Println("Server forced to shutdown:", err)
+		log.Println("Server forced to shutdown: ", err)
 	}
+
 	log.Println("Server exiting")
 }
