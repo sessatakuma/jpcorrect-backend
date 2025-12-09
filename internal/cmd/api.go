becd76d137b3aaec2f807488fd3bc1f418f997d2
Wei De Chien <wade00754@gmail.com>
2025-12-09 23:08:55 +0800

    feat(cmd): rename `StartAPI` to `Execute` and implement graceful shutdown

--- internal/cmd/api.go
+++ internal/cmd/api.go
@@ -3,7 +3,11 @@ package cmd
 import (
 	"context"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"jpcorrect-backend/internal/api"
 
@@ -11,7 +15,7 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func StartAPI() {
+func Execute() {
 	dbpool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatalf("failed to connect to database: %v", err)
@@ -23,5 +27,33 @@ func StartAPI() {
 
 	r := gin.Default()
 	api.Register(r, a)
-	r.Run() // listen and serve on
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r.Handler(),
+	}
+
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	// kill (no params) by default sends syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be caught, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Println("Server forced to shutdown:", err)
+	}
+	log.Println("Server exiting")
 }
