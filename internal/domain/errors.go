29c6d896732b10bee8b97f63f8cdc2869bf48805
torrid-fish <justinwu6900942@gmail.com>
2026-01-25 16:52:44 +0800

    feat: jwt signing keys

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -5,4 +5,6 @@ import "errors"
 var (
 	ErrNotFound       = errors.New("record not found")
 	ErrDuplicateEntry = errors.New("duplicate entry")
+	ErrUnauthorized   = errors.New("unauthorized")
+	ErrForbidden      = errors.New("forbidden")
 )
